e538cb78d39c5d908289c4ff29ddfdf36d181ca2
Shiyu Wang <shiyu3169@gmail.com>
2019-07-10 11:55:06 -0400

    add player

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
   import Navbar from "./Navbar.svelte";
   import Player from "./Player.svelte";
-
+  import AddPlayer from "./AddPlayer.svelte";
   let players = [
     {
       name: "John Doe",
@@ -16,10 +16,16 @@
       points: 34
     }
   ];
+
+  const addPlayer = e => {
+    const newPlayer = e.detail;
+    players = [...players, newPlayer];
+  };
 </script>
 
 <Navbar />
 <div class="container">
+  <AddPlayer on:addplayer={addPlayer} />
   {#if players.length === 0}
     <p>No Players</p>
   {:else}
